0b8617a8cf462b3ff5626b119e4d162aa36a6209
Jeremiah <jeremiah@shootproof.com>
2021-08-08 17:16:21 -0400

    Use new service

--- src/SimpleValuation.js
+++ src/SimpleValuation.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Card, CardContent, Typography, Grid, makeStyles } from '@material-ui/core';
-import DcaCalculatorService from './service/DcaCalculator';
+import { getCurrentValue, getInvestmentAmount, getPossibleGrowthPercent } from './service/DcaCalculator';
 
 const useStyles = makeStyles({
   root: {
@@ -9,26 +9,20 @@ const useStyles = makeStyles({
 });
 
 const SimpleValuation = (props) => {
-  const [investmentAmount, setInvestmentAmount] = useState(0);
-  const [currentValue, setCurrentValue] = useState(0);
+  const [investmentAmount, setInvestmentAmount] = useState(0.0);
+  const [currentValue, setCurrentValue] = useState(0.0);
   const [possibleGrowthPercent, setPossibleGrowthPercent] = useState(0);
 
   useEffect(() => {
     const fetchData = async () => {
-      setInvestmentAmount(await DcaCalculatorService.getInvestmentAmount());
-      setCurrentValue(await DcaCalculatorService.getCurrentValue());
-      setPossibleGrowthPercent(await DcaCalculatorService.getPossibleGrowthPercent());
+      setInvestmentAmount(await getInvestmentAmount());
+      setCurrentValue(await getCurrentValue());
+      setPossibleGrowthPercent(await getPossibleGrowthPercent());
     }
     fetchData();
   }, []);
 
   const classes = useStyles();
-
-  const formatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 2
-  });
 
   return (
     <React.Fragment>
@@ -37,12 +31,12 @@ const SimpleValuation = (props) => {
           <Grid container spacing={3}>
             <Grid item xs>
               <Typography>
-                Investment Amount : {formatter.format(investmentAmount)}
+                Investment Amount : ${Math.trunc(investmentAmount * 100) / 100}
               </Typography>
             </Grid>
             <Grid item xs>
               <Typography>
-                Current Value : {formatter.format(currentValue)}
+                Current Value : ${Math.trunc(currentValue * 100) / 100}
               </Typography>
             </Grid>
             <Grid item xs>
