6d40f740129f7033d69c4aa900ec0c31b2e1517a
Jeremiah <jeremiah@shootproof.com>
2021-08-08 13:34:59 -0400

    Get data in service

--- src/service/DcaCalculator.js
+++ src/service/DcaCalculator.js
@@ -1,15 +1,38 @@
-const DcaCalculator = {
-  getInvestmentAmount: () => {
-    return 5;
+import Papa from 'papaparse';
+
+const DcaCalculatorService = {
+  getInvestmentAmount: async () => {
+    const rows = await DcaCalculatorService.getData();
+    let investmentAmount = 0;
+
+    rows.data.forEach((item, index) => {
+      if (item[0] === 'purchase') {
+        investmentAmount += parseInt(item[3]);
+      }
+    });
+
+    return investmentAmount;
   },
 
-  getCurrentValue: () => {
+  getCurrentValue: async () => {
     return 7;
   },
 
-  getPossibleGrowthPercent: () => {
+  getPossibleGrowthPercent: async () => {
     return 2;
+  },
+
+  getData: async () => {
+    console.log('GETTING DATA');
+    const response = await fetch('/data/transfers.csv');
+    const reader = response.body.getReader();
+    const result = await reader.read();
+    const decoder = new TextDecoder('utf-8');
+    const csv = decoder.decode(result.value);
+
+    const data = Papa.parse(csv);
+    return data;
   }
 };
 
-export default DcaCalculator;
+export default DcaCalculatorService;
