9c8af682fb722004ed9efb5a8f7d214afd9a70e4
Jeremiah <jeremiah@shootproof.com>
2021-08-13 20:59:30 -0400

    Create and use new PurchasePlot component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import './App.css';
 import SimpleValuationContainer from './container/SimpleValuationContainer';
+import PurchasePlot from './component/PurchasePlot';
 import { createTheme, ThemeProvider, CssBaseline } from '@material-ui/core';
 
 function App() {
   const theme = createTheme({
     palette: {
-      type: 'dark',
+      type: 'light',
     },
   });
 
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline>
+        <PurchasePlot />
         <SimpleValuationContainer />
       </CssBaseline>
     </ThemeProvider>
