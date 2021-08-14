0a9f735bc49f54b32656da225e9c87f10664b057
Jeremiah <jeremiah@shootproof.com>
2021-08-14 17:32:34 -0400

    Add grid to app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import SimpleValuationContainer from './container/SimpleValuationContainer';
 import PurchasePlotContainer from './container/PurchasePlotContainer';
-import { ThemeProvider, CssBaseline, Switch, FormControlLabel, createTheme } from '@material-ui/core';
+import { ThemeProvider, CssBaseline, Switch, FormControlLabel, createTheme, Grid } from '@material-ui/core';
 import { useState } from 'react';
 
 function App() {
@@ -27,16 +27,26 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline>
-        <FormControlLabel
-          control={
-            <Switch
-              onChange={handleToggleTheme}
-              checked={darkTheme}
-              color="primary" />
-          }
-          label="Dark Mode"/>
-        <PurchasePlotContainer />
-        <SimpleValuationContainer />
+        <Grid container spacing={3}>
+          <Grid item xs={10} />
+          <Grid item xs={2} style={{textAlign: 'right'}}>
+            <FormControlLabel
+              control={
+                <Switch
+                  onChange={handleToggleTheme}
+                  checked={darkTheme}
+                  color="primary" />
+              }
+              label="Dark Mode"/>
+          </Grid>
+          <Grid item xs={12}>
+            <PurchasePlotContainer />
+          </Grid>
+          <Grid item xs={12}>
+            <SimpleValuationContainer />
+          </Grid>
+        </Grid>
+
       </CssBaseline>
     </ThemeProvider>
   );
