8fa24a6c731d4502631b0591994bf5eb864df6ef
Jeremiah <jtowe1@asu.edu>
2022-07-20 18:50:32 -0400

    Remove unused methods

--- src/service/DcaCalculator.tsx
+++ src/service/DcaCalculator.tsx
@@ -1,20 +1,5 @@
-import Papa from 'papaparse';
 import { getBitcoinPrice } from './BitcoinPrice';
 import { ISwanCsvRow } from '../types/swancsv'
-
-export const getData = async () => {
-  console.log('GETTING DATA');
-  const body = await (await fetch('/data/transfers.csv')).body;
-  if (body) {
-    const reader = body.getReader();
-    const result = await reader.read();
-    const decoder = new TextDecoder('utf-8');
-    const csv = decoder.decode(result.value);
-
-    const data = Papa.parse(csv);
-    return data;
-  }
-};
 
 export const getCurrentValue = async (rows: ISwanCsvRow[]) => {
   const btcValue = await getBitcoinPrice();
--- src/service/SwanBtcCsv.js
+++ src/service/SwanBtcCsv.js
@@ -1,12 +1,13 @@
 import Papa from 'papaparse';
 
 export const getCsvData = async () => {
-  console.log('GETTING DATA');
+  console.log('Loading csv...');
   const response = await fetch('/data/transfers.csv');
   const reader = response.body.getReader();
   const result = await reader.read();
   const decoder = new TextDecoder('utf-8');
   const csv = decoder.decode(result.value);
+  console.log('csv loaded');
 
   const { data, meta: { fields } } = Papa.parse(csv, {
     header: true
